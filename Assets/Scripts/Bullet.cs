a597e921a24ec890d3a095afa6f2a0276627fc80
Kaylon-Riordan <kaylonriordan@gmail.com>
2023-10-15 23:29:50 +0100

    fixed collision bug on bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,28 +6,34 @@ public class Bullet : MonoBehaviour
 {
     private float speed = 12f;
     private bool hit;
-    private CircleCollider2D coll;
+    private float decay;
+    [SerializeField] private CircleCollider2D coll;
     private Animator anim;
     private Rigidbody2D body;
-    private PlayerMovement playerMovement;
 
     // Awake method is called when the script is loaded
     void Awake()
     {
         coll = GetComponent<CircleCollider2D>();
         body = GetComponent<Rigidbody2D>();
-        playerMovement = GetComponent<PlayerMovement>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
+        anim = GetComponent<Animator>();
         body.velocity = transform.right * speed;
+        coll.enabled = true;
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)
+    private void Update()
+    {
+        if (hit) return;
+
+        decay += Time.deltaTime;
+        if (decay > 5) gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         hit = true;
         anim.SetTrigger("Hit");
+        body.velocity = Vector2.zero;
+        transform.Rotate(180f, 0f, 0f);
     }
 }
