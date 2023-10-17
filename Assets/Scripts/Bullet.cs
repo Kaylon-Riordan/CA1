25ec2bf045c423a03b6d61237765c9ffb7ef6f2d
Kaylon-Riordan <kaylonriordan@gmail.com>
2023-10-17 23:44:28 +0100

    Fixed animation for bullet collision, and commented scripts

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    // Declare a float which is used to decide the bullets speed
     private float speed = 12f;
-    private bool hit;
+    //Declare a float which is used to measure how long the bullet has existed so it can be deleted after a certain time
     private float decay;
-    [SerializeField] private CircleCollider2D coll;
+    // Declare variables which will be used to link collider, animator and rigid body between script and engine
+    private CircleCollider2D coll;
     private Animator anim;
     private Rigidbody2D body;
 
     // Awake method is called when the script is loaded
     void Awake()
     {
+        // Links the variables in script to the matching components in engine
         coll = GetComponent<CircleCollider2D>();
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        // Sets the velocity of the bullet to the speed value
         body.velocity = transform.right * speed;
-        coll.enabled = true;
     }
 
+    // This method is based off the following video (Pandemonium (2021). Unity 2D Platformer for Complete Beginners - #4 SHOOTING. YouTube. Available at: https://www.youtube.com/watch?v=PUpC44Q64zY&list=PLgOEwFbvGm5o8hayFB6skAfa8Z-mw4dPV&index=4 [Accessed 17 Oct. 2023].)
+    // Update is called once per frame
     private void Update()
     {
-        if (hit) return;
-
+        // Increase the decay variable by delta time, which is a rough calculation of every second
         decay += Time.deltaTime;
-        if (decay > 5) gameObject.SetActive(false);
+        // If 5 seconds pass, destroy the bullet
+        if (decay > 5) Destroy(gameObject);
     }
 
+    // This method is called whenever the bullets collider collides with another collider
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        hit = true;
-        anim.SetTrigger("Hit");
+        // Set the hit animaton trigger when the bullet hits something
+        anim.SetTrigger("hit");
+        // Stop the bullets velocity when it hits something
         body.velocity = Vector2.zero;
-        transform.Rotate(180f, 0f, 0f);
+        // Destroy the bullet half a second after it hits something
+        Invoke ("Destroy", 0.5f);
+    }
+
+    // This method is called to destroy the bullet when it hits something
+    public void Destroy()
+    {
+        Destroy(gameObject);
     }
 }
--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -4,34 +4,44 @@ using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
 {
+    // Declare variables which will be used to link  animator, player movement script, spawn point for bullets and bullet prefab between script and engine
     private Animator anim;
     private PlayerMovement playerMovement;
-    private float shootCooldown = 3f;
-    private float shootTimer = 10f;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private Transform gun;
+    // Declare variables to store time since last shot and the amount of time between shots
+    private float shootCooldown = 1f;
+    private float shootTimer = 10f;
 
     // Awake method is called when the script is loaded
     void Awake()
     {
+        // Links the variables in script to the matching components in engine
         anim = GetComponent<Animator>();
         playerMovement = GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
+    // This method is based off the following video (Pandemonium (2021). Unity 2D Platformer for Complete Beginners - #4 SHOOTING. YouTube. Available at: https://www.youtube.com/watch?v=PUpC44Q64zY&list=PLgOEwFbvGm5o8hayFB6skAfa8Z-mw4dPV&index=4 [Accessed 17 Oct. 2023].)
     void Update()
     {
+        // If left click is pressed, more time has passed since shooting than the cooldown, and the player is grounded, and th player isnt sliding, then shoot
         if(Input.GetMouseButton(0) && shootTimer>shootCooldown && playerMovement.isGrounded() && playerMovement.isSliding == false)
         {
             Shoot();
         }
+        // Increase the timer variable by delta time, which is a rough calculation of every second
         shootTimer += Time.deltaTime;
     }
 
+    // This method is based off the following video (Games, R. (2021). Unity: 2D Shooting. YouTube. Available at: https://www.youtube.com/watch?v=vkKulG71Yzo [Accessed 17 Oct. 2023].)
     private void Shoot()
     {
+        // Reset the timer to 0
         shootTimer = 0f;
+        // Play the shoot animation
         anim.SetTrigger("shoot");
+        // Create a copy of the bullet prefab
         Instantiate(bulletPrefab, gun.position, transform.rotation);
     }
 }
