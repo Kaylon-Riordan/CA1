bf9adc9076e66b622c90bc35236a713b47df1219
Kaylon-Riordan <kaylonriordan@gmail.com>
2023-10-20 17:24:51 +0100

    added traps that damage player and removed the damage player test method

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    // Set variables for max health, current health and an animator
     [SerializeField] private float maxHealth = 3;
     public float currentHealth;
+    private Animator anim;
 
     // Awake method is called when the script is loaded
     void Awake()
     {
+        // Set the animator to the player's animator, and set current health to start at max health
+        anim = GetComponent<Animator>();
         currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            TakeDamage(1);
-        }
-    }
-
+    // Creates a public method to take damage that will be called in other scripts
+    // This method is based on code in this video (Pandemonium (2021). Unity 2D Platformer for Complete Beginners - #7 HEALTH SYSTEM. YouTube. Available at: https://www.youtube.com/watch?v=yxzg8jswZ8A&list=PLgOEwFbvGm5o8hayFB6skAfa8Z-mw4dPV&index=7 [Accessed 20 Oct. 2023].)
     public void TakeDamage(float damage)
     {
+        // Set current health to a clamp, which has a minimum value of 0 and a maximum value of the max health varialbe, and take away however much damage was inflicted from the current health
         currentHealth = Mathf.Clamp(currentHealth - damage, 0, maxHealth);
+        // Trigger the hurt animation
+        anim.SetTrigger("hurt");
     }
-
 }
